03747736c33f4d074481d4bdc3e1b02c230098d3
akanksha <akanksha.sood08@gmail.com>
2025-11-21 12:21:11 +1100

    test pulling from database on github pages

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,11 +1,30 @@
 import { Link } from "react-router";
-
+import React, { useEffect, useState } from "react";
+import { db } from "..firebase.js";
+import { ref, onValue } from "firebase/database";
 export function Landing() {
+    const [value, setValue] = useState("");
+    useEffect(() => {
+        // Reference to /test in the database
+        const testRef = ref(db, "test");
+
+        // Listen for real-time updates
+        onValue(testRef, (snapshot) => {
+        const data = snapshot.val();
+        setValue(data);
+        });
+    }, []);
+
     return (
         <>
-            <h1>Landing Page</h1>
+            <div style={{ padding: "30px" }}>
+                <h1>Firebase Test</h1>
+                <p>Value from database:</p>
+                <pre>{JSON.stringify(value, null, 2)}</pre>
+            </div>
             <Link to="/slotName">SLOT</Link>
             <Link to="/password">Host</Link>
         </>
     );
 }
+
